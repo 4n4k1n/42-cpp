a7ee3a105e077bb0e774b225dfa16f2191bf7071
4n4k1n <anakin@pregitzer.de>
2025-11-25 16:51:22 +0100

    add: constructors andd setName function

--- CPP01/ex01/src/Zombie.cpp
+++ CPP01/ex01/src/Zombie.cpp
@@ -4,6 +4,13 @@
 
 Zombie::Zombie(const std::string &name) : _name(name) {}
 
+Zombie::Zombie() : _name("Zombie") {}
+
+void	Zombie::setName(const std::string &name)
+{
+	_name = name;
+}
+
 void	Zombie::announce(void)
 {
 	if (_name == "Foo")
