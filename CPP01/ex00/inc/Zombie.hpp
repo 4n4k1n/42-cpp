8275ec61415f1642c1551013e492ed293b942c41
4n4k1n <anakin@pregitzer.de>
2025-11-25 16:42:11 +0100

    add: default constructor

--- CPP01/ex00/inc/Zombie.hpp
+++ CPP01/ex00/inc/Zombie.hpp
@@ -6,9 +6,11 @@ class Zombie
 	private:
 		std::string _name;
 	public:
+		Zombie();
 		Zombie(const std::string &name);
 		~Zombie() = default;
 		void	announce(void);
+		void	setName(const std::string &name);
 };
 
 Zombie*	newZombie(std::string name);
