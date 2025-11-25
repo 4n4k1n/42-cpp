8e665176264d3358d796e6faf17c295c48e0a2c3
4n4k1n <anakin@pregitzer.de>
2025-11-25 14:53:18 +0100

    add: Zombie class

---
+++ CPP01/ex01/src/Zombie.cpp
@@ -0,0 +1,13 @@
+#include "Zombie.hpp"
+#include "color.hpp"
+#include <iostream>
+
+Zombie::Zombie(const std::string &name) : _name(name) {}
+
+void	Zombie::announce(void)
+{
+	if (_name == "Foo")
+		std::cout << RED << "Foo: BraiiiiiiinnnzzzZ..." << RESET << std::endl;
+	else
+		std::cout << RED << "<" << _name << ">: BraiiiiiiinnnzzzZ..." << RESET << std::endl;
+}
