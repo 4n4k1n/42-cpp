f7aeb9b09594ebfc3b387a4055908384ccc2a4aa
4n4k1n <anakin@pregitzer.de>
2025-11-30 12:37:30 +0100

    add: non default constructor

--- CPP01/ex03/inc/Weapon.hpp
+++ CPP01/ex03/inc/Weapon.hpp
@@ -1,11 +1,13 @@
+#pragma once
 #include <string>
 
 class Weapon
 {
 	private:
-		std::string _type;
+		std::string _type = "Weapon";
 	public:
 		Weapon() = default;
+		Weapon(const std::string &type);
 		~Weapon() = default;
 		const std::string	&getType(void);
 		void				setType(const std::string &type);
--- CPP01/ex03/src/Weapon.cpp
+++ CPP01/ex03/src/Weapon.cpp
@@ -1,5 +1,7 @@
 #include "Weapon.hpp"
 
+Weapon::Weapon(const std::string &type) : _type(type) {}
+
 const std::string &Weapon::getType(void)
 {
 	return (_type);
