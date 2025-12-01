8f0dfbf2eb34822d62e83a2ef48d537a208b857c
4n4k1n <anakin@pregitzer.de>
2025-12-01 14:49:24 +0100

    add: resolve exercice by makeing a recursive call

---
+++ CPP01/ex06/src/Harl.cpp
@@ -0,0 +1,38 @@
+#include "Harl.hpp"
+#include "color.hpp"
+#include <iostream>
+
+void Harl::complain(std::string level)
+{
+    for (int i = 0; i < 4; i++)
+	{
+        if (_levels[i] == level)
+		{
+            (this->*_funcs[i])();
+			if (level != _levels[3])
+				this->complain(_levels[i + 1]);
+            return;
+        }
+    }
+	std::cerr << RED << "Unknown level!" << RESET << std::endl;
+}
+
+void	Harl::debug(void)
+{
+	std::cout << YELLOW << MSG_DEBUG << RESET << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << YELLOW << MSG_INFO << RESET << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << YELLOW << MSG_WARNING << RESET << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << YELLOW << MSG_ERROR << RESET << std::endl;
+}
