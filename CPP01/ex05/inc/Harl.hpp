91cc8bcb42dd63b65cd3e8cdd5cbdb71cdd16419
4n4k1n <anakin@pregitzer.de>
2025-12-01 14:45:41 +0100

    add: msg type

--- CPP01/ex05/inc/Harl.hpp
+++ CPP01/ex05/inc/Harl.hpp
@@ -1,10 +1,10 @@
 #pragma once
 #include <string>
 
-#define MSG_DEBUG "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger.I really do !"
-#define MSG_INFO "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
-#define MSG_WARNING "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."
-#define MSG_ERROR "This is unacceptable! I want to speak to the manager now."
+#define MSG_DEBUG "[DEBUG] I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger.I really do !"
+#define MSG_INFO "[INFO] I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+#define MSG_WARNING "[WARNING] I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."
+#define MSG_ERROR "[ERROR] This is unacceptable! I want to speak to the manager now."
 
 class Harl
 {
