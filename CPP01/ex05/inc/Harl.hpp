db45528413644b971564682fd5ed08708f683e47
4n4k1n <anakin@pregitzer.de>
2025-12-01 14:14:56 +0100

    add: Harl class

---
+++ CPP01/ex05/inc/Harl.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+
+#define MSG_DEBUG "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger.I really do !"
+#define MSG_INFO "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+#define MSG_WARNING "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."
+#define MSG_ERROR "This is unacceptable! I want to speak to the manager now."
+
+class Harl
+{
+	private:
+		std::string _levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+		void (Harl::*_funcs[4])() = {
+			&Harl::debug, 
+			&Harl::info, 
+			&Harl::warning, 
+			&Harl::error};
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+	public:
+		Harl() = default;
+		~Harl() = default;
+		void complain(std::string level);
+};
