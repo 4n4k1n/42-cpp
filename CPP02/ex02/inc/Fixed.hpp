eae4e55b964026161b8ae40c5f1cc9269d976786
4n4k1n <anakin@pregitzer.de>
2025-12-02 17:56:13 +0100

    add: Makefile and colors
    add: adjusted the Fixed class

---
+++ CPP02/ex02/inc/Fixed.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include <iostream>
+
+class Fixed
+{
+	private:
+		int                 _fixed_point_value;
+		static const int    _fractional_bits = 8;
+
+	public:
+		Fixed();
+		Fixed(const int fix_int);
+		Fixed(const float fix_float);
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& other);
+		~Fixed();
+
+		int getRawBits() const;
+		void setRawBits(int const raw);
+		float toFloat() const;
+		int		toInt() const;
+		float	getFloatPart(void) const;
+		int		getIntPart(void) const;
+
+		bool	operator<(const Fixed &fixed) const;
+		bool	operator>(const Fixed &fixed) const;
+		bool	operator<=(const Fixed &fixed) const;
+		bool	operator>=(const Fixed &fixed) const;
+		bool	operator==(const Fixed &fixed) const;
+		bool	operator!=(const Fixed &fixed) const;
+
+		Fixed operator+(const Fixed &fixed) const;
+		Fixed operator-(const Fixed &fixed) const;
+		Fixed operator*(const Fixed &fixed) const;
+		Fixed operator/(const Fixed &fixed) const;
+
+		Fixed &operator++();
+		Fixed &operator--();
+		Fixed operator++(int);
+		Fixed operator--(int);
+
+		static Fixed &min(Fixed &a, Fixed &b);
+		static const Fixed &min(const Fixed &a, const Fixed &b);
+		static Fixed &max(Fixed &a, Fixed &b);
+		static const Fixed &max(const Fixed &a, const Fixed &b);
+};
+
+std::ostream	&operator<<(std::ostream &out, const Fixed &fixed);
+
